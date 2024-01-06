333efc370eaa55d00258551573a447a541e20a79
Saran Kumar <sarancodes@gmail.com>
2024-01-06 08:35:10 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
-console.log(process.env.REACT_APP_APP_ID);
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
